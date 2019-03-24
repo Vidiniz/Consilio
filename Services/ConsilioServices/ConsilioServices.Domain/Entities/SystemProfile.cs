95815fa8494c16723b5fc4faa4c2fe1c2e987d23
Vinicius <viniciusdiniz3@gmail.com>
2019-03-24 20:32:57 -0300

    inclusão das classes de dominio

---
+++ Services/ConsilioServices/ConsilioServices.Domain/Entities/SystemProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsilioServices.Domain.Entities
+{
+    public class SystemProfile
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool HasAdmin { get; set; }
+
+        public IEnumerable<SystemUser> SystemUsers { get; set; }
+    }
+}
---
+++ Services/ConsilioServices/ConsilioServices.Domain/Entities/SystemUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsilioServices.Domain.Entities
+{
+    public class SystemUser
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Status { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public int SystemProfileId { get; set; }
+
+        public SystemProfile SystemProfile { get; set; }
+    }
+}
