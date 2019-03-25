85d81d498293e89934c0cde8cb4ff28612eb9e97
Vinicius <viniciusdiniz3@gmail.com>
2019-03-25 12:03:09 -0300

    Inclusão das controllers de cadastro de usuario e cadastro de perfil

---
+++ Services/ConsilioServices/ConsilioServices.ServiceApp/Controllers/SystemProfileController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using ConsilioServices.Application.Interfaces;
+using ConsilioServices.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsilioServices.ServiceApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemProfileController : ControllerBase
+    {
+        private readonly ISystemProfileAppService _systemProfileAppService;
+
+        public SystemProfileController(ISystemProfileAppService systemProfileAppService)
+        {
+            _systemProfileAppService = systemProfileAppService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_systemProfileAppService.GetAll());
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Erros = ex.Message });
+            }
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var result = _systemProfileAppService.GetById(id);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Erros = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetByName(string value)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(value))
+                    return BadRequest(new { Error = "Parametro é obrigatório" });
+
+                var result = _systemProfileAppService.GetByName(value);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] SystemProfileViewModel value)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _systemProfileAppService.Save(value);
+                    return NoContent();
+                }
+
+                return BadRequest(new { Errors = ModelState });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] SystemProfileViewModel value)
+        {
+            try
+            {
+                var result = _systemProfileAppService.GetById(id);
+
+                if (result == null)
+                    return NotFound();
+
+                if (ModelState.IsValid)
+                {
+                    _systemProfileAppService.Update(value);
+                    return NoContent();
+                }
+
+                return BadRequest(new { Errors = ModelState });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var result = _systemProfileAppService.GetById(id);
+
+                if (result == null)
+                    return NotFound();
+
+                _systemProfileAppService.Remove(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }
+    }
+}
---
+++ Services/ConsilioServices/ConsilioServices.ServiceApp/Controllers/SystemUserController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using ConsilioServices.Application.Interfaces;
+using ConsilioServices.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsilioServices.ServiceApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemUserController : ControllerBase
+    {
+        private readonly ISystemUserAppService _systemUserAppService;
+
+        public SystemUserController(ISystemUserAppService systemUserAppService)
+        {
+            _systemUserAppService = systemUserAppService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_systemUserAppService.GetAll());
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }            
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var result = _systemUserAppService.GetById(id);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }            
+        }
+
+        [HttpGet]
+        public IActionResult GetByName(string name)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(name))
+                    return BadRequest(new { Error = "Parametro é obrigatório" });
+
+                var result = _systemUserAppService.GetByName(name);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]SystemUserViewModel  value)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _systemUserAppService.Save(value);
+                    return NoContent();
+                }
+                
+                return BadRequest(new { Errors = ModelState });
+                
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] SystemUserViewModel value)
+        {
+            try
+            {
+                var result = _systemUserAppService.GetById(id);
+
+                if (result == null)
+                    return NotFound();
+
+                if (ModelState.IsValid)
+                {
+                    _systemUserAppService.Update(value);
+                    return NoContent();
+                }
+
+                return BadRequest(new { Errors = ModelState });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var result = _systemUserAppService.GetById(id);
+
+                if (result == null)
+                    return NotFound();
+
+                _systemUserAppService.Remove(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Errors = ex.Message });
+            }
+        }        
+    }
+}
